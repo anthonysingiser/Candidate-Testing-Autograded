d14f6840702201f69d9e88227f5a634380ffc8fc
Anthony <asingiser@gmail.com>
2024-01-21 11:51:57 -0500

    working part 2

--- candidate-testing.js
+++ candidate-testing.js
@@ -11,9 +11,23 @@ let candidateAnswer = '';
 
 
 //TODO: Variables for Part 2
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = [
+  'Who was the first American Woman in space? ', 
+  'True or false: 5 kilometers == 5000 meters? ', 
+  '(5 + 3)/2 * 10 = ? ',
+  "Given the array [8,'Orbit', 'Trajectory', 45], what entry is at index 2 ? ",
+  'What is the minimum crew size for the ISS? '
+];
+
+let correctAnswers = [
+  'Sally Ride',
+  'true',
+  '40',
+  'Trajectory',
+  3
+];
+
+let candidateAnswers = [];
 
 
 function askForName() {
@@ -23,19 +37,23 @@ function askForName() {
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  candidateAnswer = input.question(question);
-
+  let answer = '';
+  for (q in questions){
+     answer = input.question(questions[q]);
+     candidateAnswers.push(answer)
+    }
 }
 
 function gradeQuiz(candidateAnswers) {
 
-  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-  if (candidateAnswer === correctAnswer) {
-    console.log("Correct!");  
-  } else {
-    console.log("Incorrect!");
-  }  
-
+  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly //   
+  for (a in candidateAnswers){
+    if(candidateAnswers[a].toLowerCase() == correctAnswers[a].toLowerCase()){
+      console.log(`${candidateAnswers[a]} = ${correctAnswers[a]}`)
+    } else {
+      console.log(`${candidateAnswers[a]} != ${correctAnswers[a]}`)
+    }
+  }
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
 
   return grade;
@@ -46,7 +64,7 @@ function runProgram() {
   // TODO 1.1c: Greet candidate using their name //
   console.log(`"Hello, ${candidateName}!`);
   askQuestion();
-  gradeQuiz(this.candidateAnswers);
+  gradeQuiz(candidateAnswers);
 }
 
 // ----------- Don't write any code or change any code below this line ---------- //
