c23b8da486b6daccfe9e162bd009e9abfa8372ad
chrysanthemum <asingiser@gmail.com>
2024-01-29 19:15:59 -0500

    finish candidate testing

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,8 +3,8 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName = '';
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
+let candidateName = '';
 let question = 'Who was the first American woman in space? ';
 let correctAnswer = 'Sally Ride';
 let candidateAnswer = '';
@@ -12,8 +12,8 @@ let candidateAnswer = '';
 
 //TODO: Variables for Part 2
 let questions = [
-  "Who was the first American woman in space? ", 
-  "True or false: 5 kilometer == 5000 meters? ", 
+  "Who was the first American woman in space? ",
+  "True or false: 5 kilometer == 5000 meters? ",
   "(5 + 3)/2 * 10 = ? ",
   "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
   "What is the minimum crew size for the ISS? "
@@ -29,44 +29,57 @@ let correctAnswers = [
 
 let candidateAnswers = [];
 
-
+// TODO 1.1b: Ask for candidate's name //
 function askForName() {
-  // TODO 1.1b: Ask for candidate's name //
   candidateName = input.question("What is your name? ");
 }
 
+// TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
 function askQuestion() {
-  // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
   let answer = '';
-  for (q in questions){
-     answer = input.question(questions[q]);
-     candidateAnswers.push(answer)
-    }
+
+  questions.forEach((question) => {
+    answer = input.question(question);
+    candidateAnswers.push(answer);
+  })
+
 }
 
+// TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly //   
+//TODO 3.2 use the variable grade to calculate the candidates score.
 function gradeQuiz(candidateAnswers) {
+  let amountCorrect = 0;
 
-  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly //   
-  for (a in candidateAnswers){
-    if(candidateAnswers[a].toLowerCase() == correctAnswers[a].toLowerCase()){
-      console.log(`${candidateAnswers[a]} = ${correctAnswers[a]}`)
+  candidateAnswers.forEach((answer) => {
+    if (answer.toLowerCase() === correctAnswers[candidateAnswers.indexOf(answer)].toLowerCase()) {
+      console.log(`${answer} = ${correctAnswers[candidateAnswers.indexOf(answer)]}`)
+      amountCorrect++;
     } else {
-      console.log(`${candidateAnswers[a]} != ${correctAnswers[a]}`)
+      console.log(`${answer} != ${correctAnswers[candidateAnswers.indexOf(answer)]}`)
     }
-  }
-  let grade;  //TODO 3.2 use this variable to calculate the candidates score.
+  })
+
+  let grade = (amountCorrect) / (questions.length) * 100;
 
   return grade;
 }
 
+// TODO 1.1c: Greet candidate using their name //
 function runProgram() {
   askForName();
-  // TODO 1.1c: Greet candidate using their name //
   console.log(`"Hello, ${candidateName}!`);
   askQuestion();
-  gradeQuiz(this.candidateAnswers);
+  let grade = gradeQuiz(candidateAnswers);
+  if (grade >= 80) {
+    console.log(`>>> Overall Grade: ${grade}% <<<`);
+    console.log(`>>> Status: PASSED <<<`);
+  } else {
+    console.log(`>>> Overall Grade: ${grade}% <<<`);
+    console.log(`>>> Status: FAILED <<<`);
+  }
 }
 
+runProgram();
 // ----------- Don't write any code or change any code below this line ---------- //
 module.exports = {
   candidateName: candidateName,
